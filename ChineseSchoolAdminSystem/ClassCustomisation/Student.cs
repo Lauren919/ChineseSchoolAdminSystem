55f01b481fd194319df287b8ca1110046895e188
Lauren919 <laurenpang919@gmail.com>
2020-12-02 17:04:08 +0000

    Added overrided ToString Methods to student and teacher classes in model layer

---
+++ ChineseSchoolAdminSystem/ClassCustomisation/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChineseSchoolAdminSystem
+{
+    public partial class Student
+    {
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ ChineseSchoolAdminSystem/ClassCustomisation/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChineseSchoolAdminSystem
+{
+    public partial class Teacher
+    {
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
