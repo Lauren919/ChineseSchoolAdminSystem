f3007bd179c68fa2796ce432cbdb566be1fc61cc
Lauren919 <laurenpang919@gmail.com>
2020-12-02 10:44:47 +0000

    Added student and teacher read and add methods to business layer

--- ChineseSchoolAdminSystemBusiness/CRUDManager.cs
+++ ChineseSchoolAdminSystemBusiness/CRUDManager.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using ChineseSchoolAdminSystem;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChineseSchoolAdminSystemBusiness
 {
@@ -6,7 +9,80 @@ namespace ChineseSchoolAdminSystemBusiness
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+        }
+
+        public Student SelectedStudent { get; set; }
+        public Teacher SelectedTeacher { get; set; }
+
+
+        public void SetSelectedStudent(object selectedItem)
+        {
+            SelectedStudent = (Student)selectedItem;
+        }
+
+        public void SetSelectedTeacher(object selectedItem)
+        {
+            SelectedTeacher = (Teacher)selectedItem;
+        }
+
+
+        public List<Student> RetrieveAllStudents()
+        {
+            using (var db = new ChineseSchoolAdminSystemContext())
+            {
+                return db.Students.ToList();
+            }
+        }
+
+        public List<Teacher> RetrieveAllTeachers()
+        {
+            using (var db = new ChineseSchoolAdminSystemContext())
+            {
+                return db.Teachers.ToList();
+            }
+        }
+
+
+        public void AddStudent (int studentID, string firstName, string lastName, DateTime dateOfBirth, int age, string allergies,
+            string parentName, string parentContactNumber, string parentEmail, int classID)
+        {
+            using (var db = new ChineseSchoolAdminSystemContext())
+            {
+                var newStudent = new Student
+                {
+                    StudentId = studentID,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    DateOfBirth = dateOfBirth,
+                    Age = age,
+                    Allergies = allergies,
+                    ParentName = parentName,
+                    ParentContactNumber = parentContactNumber,
+                    ParentEmail = parentEmail,
+                    ClassId = classID
+                };
+
+                db.Students.Add(newStudent);
+                db.SaveChanges();
+            }
+        }
+
+        public void AddTeacher (int teacherID, string firstName, string lastName, string email)
+        {
+            using (var db = new ChineseSchoolAdminSystemContext())
+            {
+                var newTeacher = new Teacher
+                {
+                    TeacherId = teacherID,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email
+                };
+
+                db.Teachers.Add(newTeacher);
+                db.SaveChanges();
+            }
         }
     }
 }
