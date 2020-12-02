771bfeeb3b1fa042e4bfd0ee8a917c7ae8ebeb11
Lauren919 <laurenpang919@gmail.com>
2020-12-02 14:53:12 +0000

    Corrected error in mainwindow and added student and teacher pages

--- ChineseSchoolAdminSystemGUI/Teachers.xaml.cs
+++ ChineseSchoolAdminSystemGUI/Teachers.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,11 +14,11 @@ using System.Windows.Shapes;
 namespace ChineseSchoolAdminSystemGUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Teachers.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class Teachers : Page
     {
-        public MainWindow()
+        public Teachers()
         {
             InitializeComponent();
         }
