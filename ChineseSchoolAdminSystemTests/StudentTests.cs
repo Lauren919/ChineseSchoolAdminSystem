187168e50d1326de48c4f0c4c8258a02f237cf8c
Lauren919 <laurenpang919@gmail.com>
2020-12-02 13:33:06 +0000

    Added Unit tests for AddStudent method

--- ChineseSchoolAdminSystemTests/StudentTests.cs
+++ ChineseSchoolAdminSystemTests/StudentTests.cs
@@ -51,9 +51,39 @@ namespace ChineseSchoolAdminSystemTests
         }
 
         [Test]
-        public void Test1()
+        public void NumberOfStudentsIncreasesBy1WhenNewStudentIsAdded()
         {
-            Assert.Pass();
+            using (var db = new ChineseSchoolAdminSystemContext())
+            {
+                var numberOfStudentsBefore = db.Students.Count();
+                _crudManager.AddStudent(7, "Lauren", "Pang", 22, "N/A", "Emy Chung", "07123112233", "EChung@gmail.com", 6);
+                var numberOfStudentsAfter = db.Students.Count();
+                Assert.AreEqual(numberOfStudentsAfter, numberOfStudentsBefore + 1);
+            }
+        }
+
+        [Test]
+        public void CheckAllInformationIsCorrectedWhenNewStudentIsAdded()
+        {
+            using (var db = new ChineseSchoolAdminSystemContext())
+            {
+                _crudManager.AddStudent(7, "Lauren", "Pang", 22, "N/A", "Emy Chung", "07123112233", "EChung@gmail.com", 6);
+
+                var newStudent = from s in db.Students
+                                  where s.StudentId == 7
+                                  select s;
+                foreach (var s in newStudent)
+                {
+                    Assert.AreEqual("Lauren", s.FirstName);
+                    Assert.AreEqual("Pang", s.LastName);
+                    Assert.AreEqual(22, s.Age);
+                    Assert.AreEqual("N/A", s.Allergies);
+                    Assert.AreEqual("Emy Chung", s.ParentName);
+                    Assert.AreEqual("07123112233", s.ParentContactNumber);
+                    Assert.AreEqual("EChung@gmail.com", s.ParentEmail);
+                    Assert.AreEqual(6, s.ClassId);
+                }
+            }
         }
     }
 }
